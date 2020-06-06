9a4e67e2c54c24814b04e54eb6b2c698f04ebfb7
Mert <mertturkmenoglu99@gmail.com>
2020-06-06 14:41:27 +0300

    Refactor VerificationActivity

--- app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/verification/VerificationActivity.kt
+++ app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/verification/VerificationActivity.kt
@@ -2,22 +2,20 @@ package io.github.mertturkmenoglu.vevericka.ui.verification
 
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import androidx.core.os.postDelayed
 import com.google.firebase.auth.FirebaseAuth
 import io.github.mertturkmenoglu.vevericka.R
 import io.github.mertturkmenoglu.vevericka.ui.login.LoginActivity
 import io.github.mertturkmenoglu.vevericka.ui.main.MainActivity
 import io.github.mertturkmenoglu.vevericka.util.FirebaseAuthHelper
 import kotlinx.android.synthetic.main.activity_verification.*
-import org.jetbrains.anko.clearTask
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.newTask
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.snackbar
 
-class VerificationActivity : AppCompatActivity() {
+class VerificationActivity : AppCompatActivity(), AnkoLogger {
     companion object {
-        private const val TAG = "VerificationActivity"
         private const val INTENT_DELAY_MILLIS = 1000L
     }
 
@@ -25,38 +23,37 @@ class VerificationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification)
 
-        verificationSendButton.setOnClickListener { view ->
-            FirebaseAuthHelper.sendVerificationEmail()
-                .addOnSuccessListener {
-                    val text = getString(R.string.verification_email_send_ok_msg)
-                    Snackbar.make(view, text, Snackbar.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener { e ->
-                    Log.e(TAG, "onCreate: ", e)
-                    val text = getString(R.string.verification_email_send_err_msg)
-                    Snackbar.make(view, text, Snackbar.LENGTH_SHORT).show()
-                }
-        }
+        verificationSendButton.setOnClickListener(::sendEmail)
+        verificationCheckButton.setOnClickListener(::checkVerification)
+        verificationSignOutButton.setOnClickListener(::signOut)
+    }
 
-        verificationCheckButton.setOnClickListener { view ->
-            FirebaseAuth.getInstance().currentUser?.reload()?.addOnSuccessListener {
-                if (FirebaseAuthHelper.isUserVerified()) {
-                    val text = getString(R.string.user_verified_ok_msg)
-                    Snackbar.make(view, text, Snackbar.LENGTH_SHORT).show()
-                    Handler().postDelayed({
-                        startActivity(intentFor<MainActivity>().newTask().clearTask())
-                    }, INTENT_DELAY_MILLIS)
-                } else {
-                    val text = getString(R.string.user_not_verified_msg)
-                    Snackbar.make(view, text, Snackbar.LENGTH_SHORT).show()
-                }
-
+    private fun sendEmail(view: View) {
+        FirebaseAuthHelper.sendVerificationEmail().addOnCompleteListener {
+            if (it.isSuccessful) {
+                view.snackbar(getString(R.string.verification_email_send_ok_msg))
+            } else {
+                error { "SendVerificationEmail failed: " + it.exception }
+                view.snackbar(getString(R.string.verification_email_send_err_msg))
             }
         }
+    }
 
-        verificationSignOutButton.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            startActivity(intentFor<LoginActivity>().newTask().clearTask())
+    private fun checkVerification(view: View) {
+        FirebaseAuth.getInstance().currentUser?.reload()?.addOnSuccessListener {
+            if (FirebaseAuthHelper.isUserVerified()) {
+                view.snackbar(getString(R.string.user_verified_ok_msg))
+                Handler().postDelayed(INTENT_DELAY_MILLIS) {
+                    startActivity(intentFor<MainActivity>().newTask().clearTask())
+                }
+            } else {
+                view.snackbar(getString(R.string.user_not_verified_msg))
+            }
         }
+    }
+
+    private fun signOut(@Suppress("UNUSED_PARAMETER") view: View) {
+        FirebaseAuth.getInstance().signOut()
+        startActivity(intentFor<LoginActivity>().newTask().clearTask())
     }
 }
