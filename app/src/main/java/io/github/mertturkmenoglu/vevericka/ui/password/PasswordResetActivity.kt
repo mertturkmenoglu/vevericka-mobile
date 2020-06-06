1f6e48d1550edbfcc1e70e415993424472a99654
Mert <mertturkmenoglu99@gmail.com>
2020-06-06 14:29:25 +0300

    Refactor PasswordResetActivity

--- app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/password/PasswordResetActivity.kt
+++ app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/password/PasswordResetActivity.kt
@@ -2,23 +2,21 @@ package io.github.mertturkmenoglu.vevericka.ui.password
 
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
+import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import androidx.core.os.postDelayed
 import io.github.mertturkmenoglu.vevericka.R
 import io.github.mertturkmenoglu.vevericka.ui.login.LoginActivity
 import io.github.mertturkmenoglu.vevericka.util.FirebaseAuthHelper
 import kotlinx.android.synthetic.main.activity_password_reset.*
-import org.jetbrains.anko.clearTask
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.newTask
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.snackbar
 
 
-class PasswordResetActivity : AppCompatActivity() {
+class PasswordResetActivity : AppCompatActivity(), AnkoLogger {
     companion object {
         private const val KEY_EMAIL = "email"
-        private const val TAG = "PasswordResetActivity"
         private const val INTENT_DELAY_MILLIS = 1000L
     }
 
@@ -30,28 +28,32 @@ class PasswordResetActivity : AppCompatActivity() {
 
         mEmailEditText = passwordResetEmailTextInput.editText ?: throw IllegalStateException()
 
-        passwordResetSendButton.setOnClickListener {
-            val email = mEmailEditText.text?.toString()?.trim() ?: return@setOnClickListener
+        passwordResetSendButton.setOnClickListener { onResetButtonClick(it) }
+    }
 
-            if (email.isBlank()) {
-                val text = getString(R.string.fill_empty_fields)
-                Snackbar.make(it, text, Snackbar.LENGTH_SHORT).show()
-                return@setOnClickListener
+    private fun onResetButtonClick(view: View) {
+        val email = mEmailEditText.text?.toString()?.trim() ?: return
+
+        if (email.isBlank()) {
+            val text = getString(R.string.fill_empty_fields)
+            view.snackbar(text)
+            return
+        }
+
+        sendEmail(view, email)
+    }
+
+    private fun sendEmail(view: View, email: String) {
+        FirebaseAuthHelper.instance.sendPasswordResetEmail(email).addOnCompleteListener {
+            if (it.isSuccessful) {
+                view.snackbar(getString(R.string.password_reset_sent_ok_msg))
+                Handler().postDelayed(INTENT_DELAY_MILLIS) {
+                    startActivity(intentFor<LoginActivity>().clearTask().newTask())
+                }
+            } else {
+                error { "SendPasswordResetEmail failed: " + it.exception }
+                view.snackbar(getString(R.string.generic_err_msg))
             }
-
-            FirebaseAuthHelper.instance.sendPasswordResetEmail(email)
-                .addOnSuccessListener { _ ->
-                    val text = getString(R.string.password_reset_sent_ok_msg)
-                    Snackbar.make(it, text, Snackbar.LENGTH_SHORT).show()
-                    Handler().postDelayed({
-                        startActivity(intentFor<LoginActivity>().clearTask().newTask())
-                    }, INTENT_DELAY_MILLIS)
-                }
-                .addOnFailureListener { e ->
-                    Log.e(TAG, "passwordReset: ", e)
-                    val text = getString(R.string.generic_err_msg)
-                    Snackbar.make(it, text, Snackbar.LENGTH_SHORT).show()
-                }
         }
     }
 
