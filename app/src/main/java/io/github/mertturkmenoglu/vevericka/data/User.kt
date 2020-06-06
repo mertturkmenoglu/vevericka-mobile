e58c767b4bcefc6a2321fa73b6e4036c5a370663
Mert <mertturkmenoglu99@gmail.com>
2020-06-06 13:32:24 +0300

    VE-7 Add user name EditText to RegisterActivity
    User's first name and last name are required for profile. User should be able to enter first & last name
    on RegisterActivity.
    
    Solves:
    - VE-7

---
+++ app/src/main/java/io/github/mertturkmenoglu/vevericka/data/User.kt
@@ -0,0 +1,3 @@
+package io.github.mertturkmenoglu.vevericka.data
+
+data class User(val firstName: String, val lastName: String, val email: String)
--- app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/register/RegisterActivity.kt
+++ app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/register/RegisterActivity.kt
@@ -3,6 +3,7 @@ package io.github.mertturkmenoglu.vevericka.ui.register
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
@@ -15,6 +16,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.snackbar.Snackbar
 import io.github.mertturkmenoglu.vevericka.R
+import io.github.mertturkmenoglu.vevericka.data.User
 import io.github.mertturkmenoglu.vevericka.ui.main.MainActivity
 import io.github.mertturkmenoglu.vevericka.util.FirebaseAuthHelper
 import io.github.mertturkmenoglu.vevericka.util.FirestoreHelper
@@ -25,18 +27,25 @@ import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.newTask
 import java.io.ByteArrayOutputStream
 
-private const val TAG = "RegisterActivity"
-private const val RC_SELECT_IMAGE = 1
-private const val KEY_EMAIL = "email"
-private const val KEY_PASSWORD = "password"
-private const val KEY_IMAGE = "image"
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var imageBytes: ByteArray
-    private var isPictureSelected = false
+    companion object {
+        private const val TAG = "RegisterActivity"
+        private const val RC_SELECT_IMAGE = 1
+        private const val KEY_FIRST_NAME = "first_name"
+        private const val KEY_LAST_NAME = "last_name"
+        private const val KEY_EMAIL = "email"
+        private const val KEY_PASSWORD = "password"
+        private const val KEY_IMAGE = "image"
+    }
 
+    private lateinit var mFirstNameEditText: EditText
+    private lateinit var mLastNameEditText: EditText
     private lateinit var mEmailEditText: EditText
     private lateinit var mPasswordEditText: EditText
+
+    private lateinit var imageBytes: ByteArray
+    private var isPictureSelected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,11 +53,13 @@ class RegisterActivity : AppCompatActivity() {
 
         registerProgressBar.visibility = GONE
 
+        mFirstNameEditText = registerFirstNameTextInput.editText ?: throw IllegalStateException()
+        mLastNameEditText = registerLastNameTextInput.editText ?: throw IllegalStateException()
         mEmailEditText = registerEmailTextInput.editText ?: throw IllegalStateException()
         mPasswordEditText = registerPasswordTextInput.editText ?: throw IllegalStateException()
 
         registerProfileImage.setOnClickListener {
-            profileImageClick()
+            selectProfileImage()
         }
 
         registerRegisterButton.setOnClickListener {
@@ -56,67 +67,69 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
+    private fun resetViews() {
+        registerProgressBar.visibility = GONE
+        registerRegisterButton.isClickable = true
+    }
+
     private fun registerClick(view: View) {
+        val firstName = mFirstNameEditText.text?.toString()?.trim() ?: return
+        val lastName = mLastNameEditText.text?.toString()?.trim() ?: return
         val email = mEmailEditText.text?.toString()?.trim() ?: return
         val password = mPasswordEditText.text?.toString()?.trim() ?: return
 
-        if (email.isBlank() || password.isBlank()) {
+        if (listOf(firstName, lastName, email, password).any { it.isBlank() }) {
             val text = getString(R.string.fill_empty_fields)
             Snackbar.make(view, text, Snackbar.LENGTH_SHORT).show()
             return
         }
 
+        val user = User(firstName, lastName, email)
+
         registerProgressBar.visibility = VISIBLE
         registerRegisterButton.isClickable = false
 
         FirebaseAuthHelper.createUser(email, password)
-            .addOnSuccessListener { onCreateUserSuccess(view, email, password) }
+            .addOnSuccessListener { onCreateUserSuccess(view, user, password) }
             .addOnFailureListener { onCreateUserFail(view, it) }
     }
 
-    private fun onCreateUserSuccess(view: View, email: String, password: String) {
+    private fun onCreateUserSuccess(view: View, user: User, password: String) {
         val okMessage = getString(R.string.user_create_ok_msg)
         Snackbar.make(view, okMessage, Snackbar.LENGTH_SHORT).show()
 
-        val signInTask = FirebaseAuthHelper.signIn(email, password)
+        FirebaseAuthHelper.signIn(user.email, password).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val uid = it.result?.user?.uid ?: System.currentTimeMillis().toString()
+                onSignInSuccess(user, uid)
+            } else {
+                Log.e(TAG, "registerClick: ", it.exception)
+                resetViews()
 
-        signInTask.addOnSuccessListener {
-            val uid = it.user?.uid ?: System.currentTimeMillis().toString()
-            onSignInSuccess(email, uid)
-        }
-
-        signInTask.addOnFailureListener {
-            Log.e(TAG, "registerClick: ", it)
-
-            registerProgressBar.visibility = GONE
-            registerRegisterButton.isClickable = true
-            val message = it.localizedMessage ?: getString(R.string.generic_err_msg)
-
-            Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show()
+                val message = it.exception?.localizedMessage ?: getString(R.string.generic_err_msg)
+                Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show()
+            }
         }
     }
 
     private fun onCreateUserFail(view: View, e: Exception) {
         Log.e(TAG, "registerClick: ", e)
+        resetViews()
 
-        registerProgressBar.visibility = GONE
-        registerRegisterButton.isClickable = true
         val message = e.localizedMessage ?: getString(R.string.generic_err_msg)
-
         Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show()
     }
 
-    private fun onSignInSuccess(email: String, uid: String) {
-        FirestoreHelper.saveUser(email)
-            .addOnSuccessListener {
-                onDatabaseSaveSuccess(uid)
+    private fun onSignInSuccess(user: User, uid: String) {
+        FirestoreHelper.saveUser(user).addOnCompleteListener {
+            if (!it.isSuccessful) {
+                Log.e(TAG, "User save failed: ", it.exception)
+                resetViews()
+                return@addOnCompleteListener
             }
-            .addOnFailureListener {
-                Log.e(TAG, "User save failed: ", it)
 
-                registerProgressBar.visibility = GONE
-                registerRegisterButton.isClickable = true
-            }
+            onDatabaseSaveSuccess(uid)
+        }
     }
 
     private fun onDatabaseSaveSuccess(uid: String) {
@@ -125,20 +138,17 @@ class RegisterActivity : AppCompatActivity() {
             return
         }
 
-        val uploadTask = StorageHelper.uploadImageByteArray(uid, imageBytes)
-
-        uploadTask.addOnSuccessListener {
-            startActivity(intentFor<MainActivity>().clearTask().newTask())
-        }
-
-        uploadTask.addOnFailureListener {
-            Log.e(TAG, "Image upload failed: ", it)
-            registerProgressBar.visibility = GONE
-            registerRegisterButton.isClickable = true
+        StorageHelper.uploadImageByteArray(uid, imageBytes).addOnCompleteListener {
+            if (it.isSuccessful) {
+                startActivity(intentFor<MainActivity>().clearTask().newTask())
+            } else {
+                Log.e(TAG, "Image upload failed: ", it.exception)
+                resetViews()
+            }
         }
     }
 
-    private fun profileImageClick() {
+    private fun selectProfileImage() {
         val intent = Intent().apply {
             type = "image/*"
             action = Intent.ACTION_GET_CONTENT
@@ -149,26 +159,28 @@ class RegisterActivity : AppCompatActivity() {
         startActivityForResult(Intent.createChooser(intent, title), RC_SELECT_IMAGE)
     }
 
+    private fun setImageBytesFromUri(uri: Uri?) {
+        val selectedBitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+        val outputStream = ByteArrayOutputStream()
+        selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        imageBytes = outputStream.toByteArray()
+        isPictureSelected = true
+    }
+
+    private fun loadImageFromBytes() {
+        Glide.with(this)
+            .load(imageBytes)
+            .override(125, 125)
+            .apply(RequestOptions().circleCrop())
+            .into(registerProfileImage)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == RC_SELECT_IMAGE && resultCode == Activity.RESULT_OK && data != null
-            && data.data != null
-        ) {
-            val imagePath = data.data
-            val selectedBmp = MediaStore.Images.Media.getBitmap(contentResolver, imagePath)
-
-            val outputStream = ByteArrayOutputStream()
-            selectedBmp.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-            imageBytes = outputStream.toByteArray()
-
-            Glide.with(this)
-                .load(imageBytes)
-                .override(125, 125)
-                .apply(RequestOptions().circleCrop())
-                .into(registerProfileImage)
-
-            isPictureSelected = true
+        if (requestCode == RC_SELECT_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            setImageBytesFromUri(data.data)
+            loadImageFromBytes()
         }
     }
 
@@ -179,6 +191,8 @@ class RegisterActivity : AppCompatActivity() {
             outState.putByteArray(KEY_IMAGE, imageBytes)
         }
 
+        outState.putString(KEY_FIRST_NAME, mFirstNameEditText.text?.toString())
+        outState.putString(KEY_LAST_NAME, mLastNameEditText.text?.toString())
         outState.putString(KEY_EMAIL, mEmailEditText.text?.toString())
         outState.putString(KEY_PASSWORD, mPasswordEditText.text?.toString())
     }
@@ -186,15 +200,12 @@ class RegisterActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
 
+        mFirstNameEditText.setText(savedInstanceState.getString(KEY_FIRST_NAME, ""))
+        mLastNameEditText.setText(savedInstanceState.getString(KEY_LAST_NAME, ""))
         mEmailEditText.setText(savedInstanceState.getString(KEY_EMAIL, ""))
         mPasswordEditText.setText(savedInstanceState.getString(KEY_PASSWORD, ""))
 
         imageBytes = savedInstanceState.getByteArray(KEY_IMAGE) ?: return
-
-        Glide.with(this)
-            .load(imageBytes)
-            .apply(RequestOptions().circleCrop())
-            .override(125, 125)
-            .into(registerProfileImage)
+        loadImageFromBytes()
     }
 }
