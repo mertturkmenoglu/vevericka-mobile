1e2bdbadf96062090009d212af2ba09cfeede08c
Mert <mertturkmenoglu99@gmail.com>
2020-06-09 11:27:40 +0300

    Update profile fragment

--- app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/main/profile/ProfileFragment.kt
@@ -6,20 +6,30 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import io.github.mertturkmenoglu.vevericka.R
 import io.github.mertturkmenoglu.vevericka.data.model.User
+import io.github.mertturkmenoglu.vevericka.ui.main.home.posts.PostAdapter
 import io.github.mertturkmenoglu.vevericka.util.FirebaseAuthHelper
 import io.github.mertturkmenoglu.vevericka.util.StorageHelper
 import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 import org.jetbrains.anko.design.snackbar
 
 class ProfileFragment : Fragment() {
+    companion object {
+        const val KEY_PROFILE_UID = "uid"
+    }
+
     private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var mRoot: View
+    private lateinit var mAdapter: PostAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,14 +38,16 @@ class ProfileFragment : Fragment() {
     ): View? {
         profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+        mRoot = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        val uid = FirebaseAuthHelper.getCurrentUserId()
+        val uid = arguments?.getString(KEY_PROFILE_UID) ?: FirebaseAuthHelper.getCurrentUserId()
         profileViewModel.getUser(uid).observe(viewLifecycleOwner, Observer {
             initViews(uid, it)
         })
 
-        return root
+        getPosts(uid)
+
+        return mRoot
     }
 
     private fun initViews(uid: String, user: User) {
@@ -45,13 +57,40 @@ class ProfileFragment : Fragment() {
         setLocation(user)
         setWebsite(user)
 
-        profilePostsButton.setOnClickListener {
-            it.snackbar(getString(R.string.profile_posts))
-        }
-
         profileFriendsButton.setOnClickListener {
             it.snackbar(getString(R.string.profile_friends))
         }
+
+        if (uid != FirebaseAuthHelper.getCurrentUserId()) {
+            profileEditProfileButton.isClickable = false
+            profileEditProfileButton.visibility = GONE
+        }
+
+        profileEditProfileButton.setOnClickListener {
+            editProfile(uid)
+        }
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        val ctx = activity?.applicationContext ?: throw IllegalStateException()
+        mRoot.profileRecyclerView.layoutManager = LinearLayoutManager(ctx)
+        mAdapter = PostAdapter(ctx)
+        mRoot.profileRecyclerView.adapter = mAdapter
+        mAdapter.setPostClickListener {
+            Toast.makeText(ctx, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun editProfile(uid: String) {
+
+    }
+
+    private fun getPosts(uid: String) {
+        profileViewModel.getPosts(uid).observe(viewLifecycleOwner, Observer {
+            mAdapter.submitList(it)
+        })
     }
 
     private fun loadProfileImage(uid: String) {
--- app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/main/profile/ProfileViewModel.kt
+++ app/src/main/java/io/github/mertturkmenoglu/vevericka/ui/main/profile/ProfileViewModel.kt
@@ -1,24 +1,20 @@
 package io.github.mertturkmenoglu.vevericka.ui.main.profile
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.ktx.toObject
+import io.github.mertturkmenoglu.vevericka.data.model.Post
 import io.github.mertturkmenoglu.vevericka.data.model.User
 import io.github.mertturkmenoglu.vevericka.util.FirestoreHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.error
 
-class ProfileViewModel : ViewModel() {
-    private companion object {
-        private const val TAG = "ProfileViewModel"
-    }
-
-    private var mUser = MutableLiveData<User>()
+class ProfileViewModel : ViewModel(), AnkoLogger {
+    private val mUser = MutableLiveData<User>()
+    private val mPosts = MutableLiveData<List<Post>>()
 
     fun getUser(uid: String): LiveData<User> {
         viewModelScope.launch(Dispatchers.IO) {
@@ -28,14 +24,29 @@ class ProfileViewModel : ViewModel() {
         return mUser
     }
 
+    fun getPosts(uid: String): LiveData<List<Post>> {
+        viewModelScope.launch(Dispatchers.IO) {
+            getPostsData(uid)
+        }
+
+        return mPosts
+    }
+
     private suspend fun getUserData(uid: String) {
         try {
-            val result = FirestoreHelper.getUserAsTask(uid).await()
-            val user = result.toObject<User>()
-            mUser.postValue(user)
-        } catch (e: FirebaseFirestoreException) {
-            Log.e(TAG, "getUserData: ", e)
+            val result = FirestoreHelper.getUser(uid)
+            mUser.postValue(result)
+        } catch (e: Exception) {
+            error { "GetUsersData failed: $e" }
         }
     }
 
+    private suspend fun getPostsData(uid: String) {
+        try {
+            val result = FirestoreHelper.getUserPosts(uid)
+            mPosts.postValue(result)
+        } catch (e: Exception) {
+            error { "GetPostsData failed: $e" }
+        }
+    }
 }
