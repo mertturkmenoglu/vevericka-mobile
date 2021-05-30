6f4104039c69cf6e93dd6ec33083cadd5d4d4de0
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2021-05-30 15:48:46 +0300

    Reset repository

--- app/src/main/java/io/github/mertturkmenoglu/vevericka/interfaces/CommentClickListener.kt
+++
@@ -1,7 +0,0 @@
-package io.github.mertturkmenoglu.vevericka.interfaces
-
-interface CommentClickListener {
-    fun onClick(uid: String)
-
-    fun onClick(action: (uid: String) -> Unit)
-}
--- app/src/main/java/io/github/mertturkmenoglu/vevericka/interfaces/PersonClickListener.kt
+++
@@ -1,7 +0,0 @@
-package io.github.mertturkmenoglu.vevericka.interfaces
-
-import io.github.mertturkmenoglu.vevericka.data.model.User
-
-interface PersonClickListener {
-    fun onClick(user: User)
-}
